7c34547fab51557121e2409474b7929386ec06bf
bilgihankose <bilgihankose@gmail.cim>
2021-06-14 20:39:02 +0300

    added all handler

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 
 	router := mux.NewRouter()
+
+	books = append(books,
+		Book{ID: 1, Title: "Breasts and Eggs",Author: "Mieko Kawakami", Year: "2020"},
+		Book{ID: 2, Title: "Where the Wild Ladies Are",Author: "Aoko Matsuda", Year: "2020"},
+		Book{ID: 3, Title: "Deacon King Kong",Author: "James McBride", Year: "2020"},
+		Book{ID: 4, Title: "A Burning",Author: "Megha Majumdar", Year: "2020"},
+		Book{ID: 5, Title: " I Hold a Wolf by the Ears",Author: "Laura van den Berg", Year: "2020"})
+
+
 	router.HandleFunc("/books", getBooks).Methods("GET")
 	router.HandleFunc("/books/{id}", getBook).Methods("GET")
 	router.HandleFunc("/books", addBook).Methods("POST")
@@ -17,8 +27,6 @@ func main() {
 	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", router)) //if has any error
-
-
 
 }
 
@@ -33,21 +41,53 @@ var books []Book // holding the book records
 
 
 func getBooks(w http.ResponseWriter, r *http.Request){
-	fmt.Println("getBooks")
+	log.Println("getBooks")
+	json.NewEncoder(w).Encode(books)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request){
-	fmt.Println("getBook")
+	params := mux.Vars(r)
+	i, _ := strconv.Atoi(params["id"]) // _ for error
+	log.Println(params)
+
+	for _, book := range(books){
+		if book.ID == i {
+			json.NewEncoder(w).Encode(&book)
+		}
+	}
 }
 
 func addBook(w http.ResponseWriter, r *http.Request){
-	fmt.Println("addBook")
+	log.Println("addBook")
+	var book Book
+	json.NewEncoder(r.Body).Encode(&book)
+	books = append(books,book)
+	json.NewEncoder(w).Encode(books)
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request){
-	fmt.Println("updateBook")
+	log.Println("updateBook")
+	var book Book
+	json.NewDecoder(r.Body).Decode(&book)
+
+	for i, item := range books{
+		if item.ID == book.ID {
+			books[i] = book
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func removeBook(w http.ResponseWriter, r *http.Request){
-	fmt.Println("removeBook")
+	log.Println("removeBook")
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"]) // _ for error
+
+	for i, item := range books{
+		if item.ID == book.ID {
+			books = append(books[:i], books[i+1:]...)
+		}
+	}
+
+	json.NewEncoder(w).Encode(books)
 }
